4e4b7d55318ae50f86f05120d9094b0a132a0917
Choe <choe@May-tinh-xach-tay-cua-Choe.local>
2024-03-14 21:55:11 +0700

    viet hoa

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const backpack_client_1 = require("./backpack_client");
+require('dotenv').config();
 
 function delay(ms) {
     return new Promise(resolve => {
@@ -44,21 +45,23 @@ let sellbuy = 0;
 
 const init = async (client) => {
     try {
-        console.log(`成功买入次数:${successbuy},成功卖出次数:${sellbuy}`);
-        console.log(getNowFormatDate(), "等待3秒...");
+        console.log(`Số lần mua hàng thành công:${successbuy}, Số lần bán hàng thành công:${sellbuy}`);
+        console.log(getNowFormatDate(), "đợi 3 giây...");
         await delay(3000);
-        console.log(getNowFormatDate(), "正在获取账户信息中...");
+        console.log(getNowFormatDate(), "Truy xuất thông tin tài khoản...");
         let userbalance = await client.Balance();
         //判断账号USDC余额是否大于5
         if (userbalance.USDC.available > 5) {
+            console.log('Chuẩn bị mua');
             await buyfun(client);
         } else {
+            console.log('Chuẩn bị bán');
             await sellfun(client);
             return;
         }
     } catch (e) {
         init(client);
-        console.log(getNowFormatDate(), "挂单失败，重新挂单中...");
+        console.log(getNowFormatDate(), "Lệnh chờ xử lý không thành công, lệnh đang được đặt lại....");
         await delay(1000);
     }
 }
@@ -66,24 +69,24 @@ const init = async (client) => {
 
 
 const sellfun = async (client) => {
-    //取消所有未完成订单
+    //Hủy tất cả các đơn hàng đang tồn đọng
     let GetOpenOrders = await client.GetOpenOrders({ symbol: "SOL_USDC" });
     if (GetOpenOrders.length > 0) {
         let CancelOpenOrders = await client.CancelOpenOrders({ symbol: "SOL_USDC" });
-        console.log(getNowFormatDate(), "取消了所有挂单");
+        console.log(getNowFormatDate(), "Tất cả các đơn đặt hàng đang chờ xử lý đã bị hủy");
     } else {
-        console.log(getNowFormatDate(), "账号订单正常，无需取消挂单");
+        console.log(getNowFormatDate(), "Lệnh tài khoản diễn ra bình thường và không cần hủy lệnh chờ xử lý.");
     }
-    console.log(getNowFormatDate(), "正在获取账户信息中...");
+    console.log(getNowFormatDate(), "Truy xuất thông tin tài khoản...");
     //获取账户信息
     let userbalance2 = await client.Balance();
-    console.log(getNowFormatDate(), "账户信息:", userbalance2);
-    console.log(getNowFormatDate(), "正在获取sol_usdc的市场当前价格中...");
+    console.log(getNowFormatDate(), "thông tin tài khoản:", userbalance2);
+    console.log(getNowFormatDate(), "Lấy giá thị trường hiện tại của sol_usdc...");
     //获取当前
     let { lastPrice: lastPriceask } = await client.Ticker({ symbol: "SOL_USDC" });
-    console.log(getNowFormatDate(), "sol_usdc的市场当前价格:", lastPriceask);
+    console.log(getNowFormatDate(), "Giá thị trường hiện tại của sol_usdc:", lastPriceask);
     let quantitys = ((userbalance2.SOL.available / 2) - 0.02).toFixed(2).toString();
-    console.log(getNowFormatDate(), `正在卖出中... 卖${quantitys}个SOL`);
+    console.log(getNowFormatDate(), `bán... 卖${quantitys}个SOL`);
     let orderResultAsk = await client.ExecuteOrder({
         orderType: "Limit",
         price: lastPriceask.toString(),
@@ -94,13 +97,13 @@ const sellfun = async (client) => {
     })
 
     if (orderResultAsk?.status == "Filled" && orderResultAsk?.side == "Ask") {
-        console.log(getNowFormatDate(), "卖出成功");
+        console.log(getNowFormatDate(), "Đã bán thành công");
         sellbuy += 1;
-        console.log(getNowFormatDate(), "订单详情:", `卖出价格:${orderResultAsk.price}, 卖出数量:${orderResultAsk.quantity}, 订单号:${orderResultAsk.id}`);
+        console.log(getNowFormatDate(), "chi tiết đặt hàng:", `gia ban:${orderResultAsk.price}, số lượng:${orderResultAsk.quantity}, id:${orderResultAsk.id}`);
         init(client);
     } else {
-        console.log(getNowFormatDate(), "卖出失败");
-        throw new Error("卖出失败");
+        console.log(getNowFormatDate(), "Bán không thành công", orderResultAsk.status);
+        throw new Error("Bán không thành công");
     }
 }
 
@@ -109,19 +112,19 @@ const buyfun = async (client) => {
     let GetOpenOrders = await client.GetOpenOrders({ symbol: "SOL_USDC" });
     if (GetOpenOrders.length > 0) {
         let CancelOpenOrders = await client.CancelOpenOrders({ symbol: "SOL_USDC" });
-        console.log(getNowFormatDate(), "取消了所有挂单");
+        console.log(getNowFormatDate(), "Tất cả các đơn đặt hàng đang chờ xử lý đã bị hủy");
     } else {
-        console.log(getNowFormatDate(), "账号订单正常，无需取消挂单");
+        console.log(getNowFormatDate(), "Lệnh tài khoản diễn ra bình thường và không cần hủy lệnh chờ xử lý.");
     }
-    console.log(getNowFormatDate(), "正在获取账户信息中...");
+    console.log(getNowFormatDate(), "Truy xuất thông tin tài khoản...");
     //获取账户信息
     let userbalance = await client.Balance();
-    console.log(getNowFormatDate(), "账户信息:", userbalance);
-    console.log(getNowFormatDate(), "正在获取sol_usdc的市场当前价格中...");
+    console.log(getNowFormatDate(), "thông tin tài khoản:", userbalance);
+    console.log(getNowFormatDate(), "Lấy giá thị trường hiện tại của sol_usdc...");
     //获取当前
     let { lastPrice } = await client.Ticker({ symbol: "SOL_USDC" });
-    console.log(getNowFormatDate(), "sol_usdc的市场当前价格:", lastPrice);
-    console.log(getNowFormatDate(), `正在买入中... 花${(userbalance.USDC.available - 2).toFixed(2).toString()}个USDC买SOL`);
+    console.log(getNowFormatDate(), "Giá thị trường hiện tại của sol_usdc:", lastPrice);
+    console.log(getNowFormatDate(), `mua ngay... 花${(userbalance.USDC.available - 2).toFixed(2).toString()}个USDC买SOL`);
     let quantitys = ((userbalance.USDC.available - 2) / lastPrice).toFixed(2).toString();
     console.log("1024", quantitys);
     let orderResultBid = await client.ExecuteOrder({
@@ -133,19 +136,19 @@ const buyfun = async (client) => {
         timeInForce: "IOC"
     })
     if (orderResultBid?.status == "Filled" && orderResultBid?.side == "Bid") {
-        console.log(getNowFormatDate(), "下单成功");
+        console.log(getNowFormatDate(), "đặt hàng thành công");
         successbuy += 1;
-        console.log(getNowFormatDate(), "订单详情:", `购买价格:${orderResultBid.price}, 购买数量:${orderResultBid.quantity}, 订单号:${orderResultBid.id}`);
+        console.log(getNowFormatDate(), "chi tiết đặt hàng:", `giá :${orderResultBid.price}, Số lượng:${orderResultBid.quantity}, id:${orderResultBid.id}`);
         init(client);
     } else {
-        console.log(getNowFormatDate(), "下单失败");
-        throw new Error("买入失败");
+        console.log(getNowFormatDate(), "Lệnh không thành công", orderResultBid.status);
+        throw new Error("Lệnh không thành công");
     }
 }
 
 (async () => {
-    const apisecret = "";
-    const apikey = "";
+    const apisecret = process.env.API_SECRET;
+    const apikey = process.env.API_KEY;
     const client = new backpack_client_1.BackpackClient(apisecret, apikey);
     init(client);
 })()
